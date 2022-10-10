68437b5ee5b17fccb6c9775c36469192a05ffe09
rivermin <rivermin.seo@gmail.com>
2022-10-11 01:44:15 +0900

    for heroku

--- src/main/java/com/example/springboard/SpringboardApplication.java
+++ src/main/java/com/example/springboard/SpringboardApplication.java
@@ -4,10 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Collections;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringboardApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringboardApplication.class, args);
+		System.getenv().forEach((k, v) -> {
+			System.out.println(k + ":" + v);
+		});
+
+		String envPort = System.getenv("PORT");
+		SpringApplication app = new SpringApplication(SpringboardApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", envPort));
+
+		app.run(args);
 	}
 }
